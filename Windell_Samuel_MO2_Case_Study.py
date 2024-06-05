8cef3943e495013ecbb9a68c510abd44ad1e6847
swindell4 <156862334+swindell4@users.noreply.github.com>
2024-06-05 17:59:17 -0400

    Add files via upload

--- Windell_Samuel_MO2_Case_Study.py
+++ Windell_Samuel_MO2_Case_Study.py
@@ -3,12 +3,13 @@
 #This program allows the user to enter names of students along with their Grade Point Average. The program then tells them whether 
 #they made the Dean's List or the Honor Roll. 
 while True: 
-   lastName = str(input("Enter a students last name or \"ZZZ\" to quit. \n")) 
+   lastName = str(input("Enter a students last name or \"ZZZ\" to quit. \n")) #This variable is intended to hold a students last name
    if lastName == "ZZZ": 
       break 
    else: 
-      firstName = str(input("Enter that student's first name. \n")) 
-      GPA = float(input("What is %s %s's GPA (Grade Point Average)? \n" % (firstName, lastName))) 
+      firstName = str(input("Enter that student's first name. \n")) #This variable is intended to hold a students first name
+      GPA = float(input("What is %s %s's GPA (Grade Point Average)? \n" % (firstName, lastName))) #This variable is intended to hold 
+#Decimal point value representing a "Grade Point Average" of the student assigned to the firstName and lastName variables. 
       if GPA >= 3.5: 
          print("%s %s made the Dean's List!" % (firstName, lastName)) 
       elif GPA >= 3.0: 
